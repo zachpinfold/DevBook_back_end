bed5dd8499b822b4b338aed16e3d5d5eacee4a62
ZachPinfold <Zach@tibbi.co.uk>
2020-07-11 15:19:57 +0100

    setup JSON middleware for auth

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
   },
   date: {
     type: Date,
-    default: date.now
+    default: Date.now
   }
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const request = require("request");
+// const request = require("request")
+const gravatar = require("gravatar");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc  Register user
@@ -16,12 +22,61 @@ router.post(
       "Please enter a password that is a minimum of 6 characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "user already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mp"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtToken"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
